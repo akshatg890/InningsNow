875f6ae714861e5e53c49d89d9e850488fc71aef
Arun <prajapatiarun742@gmail.com>
2024-12-05 15:24:08 +0530

    created component for hero section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,15 @@
+import React from "react";
+import Hero from "./components/Hero/Hero";
+import Header from "./components/Header/Header";
+
 
 function App() {
-
   return (
-    <>
-      <main className="w-screen h-screen overflow-x-hidden overflow-y-auto">
-        {/* <Header />
-        <LiveScore /> */}
-        <h1 className=" text-2xl  py-3 text-center font-bold text-custom-brown bg-custom-light-green font-content">Hello world</h1>
-        <h2 className="bg-blue-500">HIIIIIIII/......</h2>
-      </main>
-      
-    </>
-  )
+    <div>
+      <Hero />
+    </div>
+  );
+
 }
 
-export default App
+export default App;
